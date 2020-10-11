b57d48b1ebd9df088e88c7cbae2a5d95c7ec652a
Alexander Bowers <7319abowers@gmail.com>
2020-10-11 01:57:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   length = array.length
   new_numbers = []
   length.times do |squared|
-    new_numbers << squared ** 2
+    new_numbers << squared * squared
   end
   new_numbers
 end
