ac3a68d6478e7c4c5aa66b4e5e1cad8aebbc668b
Alexander Bowers <7319abowers@gmail.com>
2020-10-11 02:00:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter
   new_numbers = []
   while counter < array.length
-    new_numbers << array[counter]
+    new_numbers << array[counter] ** 2
     counter += 1
   end
   new_numbers
